2c4e70c22d9d2cb7207b89e0243d17ef2aa2fe68
Prerna237 <pr764@snu.edu.in>
2017-08-13 22:59:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//IMporting some software packages
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -5,6 +6,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+//when a get req is made to /
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -13,6 +15,17 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article1',function(req,res){
+    res.send('This is a log response to article1')
+});
+
+app.get('/article2',function(req,res){
+    res.send('This is a log response to article2')
+});
+
+app.get('/article3',function(req,res){
+    res.send('This is a log response to article3')
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
