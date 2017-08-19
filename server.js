61633e5cb156feb355840824eae3fcd83aa6977c
Prerna237 <pr764@snu.edu.in>
2017-08-19 21:51:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article1',function(req,res){
-    res.sendFile(path.join(_dirname,'article1.html'))
+    res.sendFile(path.join(__dirname,'article1.html'))
 });
 
 app.get('/article2',function(req,res){
